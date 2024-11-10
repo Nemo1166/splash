87c331158f8d121c0c76d0b256455a5e5be9c5ec
Nemo1166 <zhangguoxi1166@gmail.com>
2024-11-10 23:17:27 +0000

    add reference

--- splash/alpha.py
+++ splash/alpha.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy.typing import ArrayLike
 
+# Reference: https://doi.org/10.1098/rspa.2020.0346
 def Alpha(mi: ArrayLike, omega: float=3):
     # if mi == 0:
     #     return np.nan
